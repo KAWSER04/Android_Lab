03650817e1bf4c7e5e6860a7aa15c46367262ae9
kawser <kawser.cse.ru@gmail.com>
2019-09-27 22:01:16 +0600

    first commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,13 +1,69 @@
-package com.example.calculator;
+package com.example.simple_calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    public String str ="";
+    Character op = 'q';
+    int i,num,numtemp;
+    EditText showResult;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showResult = (EditText)findViewById(R.id.result_id);
+
+
     }
-}
+    public void btn1Clicked(View v){
+        insert(1);
+
+    }
+
+    public void btn2Clicked(View v){
+        insert(2);
+
+    }
+    public void btn3Clicked(View v){
+        insert(3);
+
+    }
+    public void btn4Clicked(View v){
+        insert(4);
+
+    }
+    public void btn5Clicked(View v){
+        insert(5);
+
+    }
+    public void btn6Clicked(View v){
+        insert(6);
+    }
+    public void btn7Clicked(View v){
+        insert(7);
+
+    }
+    public void btn8Clicked(View v){
+        insert(8);
+
+    }
+    public void btn9Clicked(View v){
+        insert(9);
+
+    }
+    public void btnplusClicked(View v){
+        perform();
+        op = '+';
+
+    }
+
+    public void btnminusClicked(View v){
+        perform();
+        op = '-';
+
+    }
